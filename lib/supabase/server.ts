afa32d05e018c9976c1b9697bd8a8529e02b4e1d
trulboy77 <trulboy77@gmail.com>
2025-09-27 03:11:30 -0400

    Update server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,7 +1,7 @@
 import { createClient as createSupabaseClient } from "@supabase/supabase-js"
 import type { Database } from "./types"
 
-export function createClient() {
+export function createServerClient() {
   return createSupabaseClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
