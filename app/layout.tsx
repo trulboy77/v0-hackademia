c0927c09ac4a638930126b401864b94c01ea4ef4
trulboy77 <trulboy77@gmail.com>
2025-09-27 11:46:30 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
-import { ThemeProvider } from "@/hooks/use-theme"
+import { useTheme } from "@/hooks/use-theme"
 import { NextIntlClientProvider } from "next-intl"
 import { getMessages } from "next-intl/server"
 
